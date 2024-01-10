75ccab902b3403b73f0ee35e23314c9c8ca5d19a
alex02r <alex.giochi02@gmail.com>
2024-01-10 16:31:09 +0100

    [UPGRADE] Add card from store.js file

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -1,11 +1,22 @@
 <script>
+import { store } from "../store";
 export default {
-    
+    data() {
+        return {
+            store
+        }
+    }
 }
 </script>
 <template lang="">
-    <div class="col-6 col-md-4 col-lg-3">
-        prova
+    <div class="col-6 col-md-4 col-lg-3"  v-for="(card, index) in store.cardsList" :key="index">
+        <div class="card">
+            <img :src="card.card_images[0].image_url" class="card-img-top" :alt="card.name">
+            <div class="card-body">
+              <h5 class="card-title">{{ card.name }}</h5>
+              <p class="card-text"> {{ card.archetype }}</p>
+            </div>
+          </div>
     </div>
 </template>
 <style lang="scss" scoped>
