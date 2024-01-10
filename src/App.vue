7e828061a6aad6c47e8ddc76294c0ca7167ba2ff
alex02r <alex.giochi02@gmail.com>
2024-01-10 15:08:33 +0100

    [UPGRADE] Add & link components for App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppCards from "./components/AppCards.vue";
 export default {
-  
+  components:{
+    AppHeader,
+    AppCards
+  }
 }
 </script>
 <template lang="">
