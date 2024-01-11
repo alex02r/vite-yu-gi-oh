8d61e6e9fb59f31dd66312009778d8f97fcd98f4
alex02r <alex.giochi02@gmail.com>
2024-01-11 15:39:59 +0100

    [UPGRADE] Add Numbers of cards show

--- src/components/AppNumberResult.vue
+++ src/components/AppNumberResult.vue
@@ -1,11 +1,21 @@
 <script>
+import { store } from "../store";
 export default {
+    data() {
+        return {
+            store
+        }
+    },
+    computed:{
+
+    }
     
 }
 </script>
 <template lang="">
-    <div calss="col-4">
-        
+    <div calss="col-4" v-if="store.select">
+        <span class="badge text-bg-secondary" v-if="store.cardsList.length > 0">{{ store.cardsList.length }} results</span>
+        <span class="badge rounded-pill text-bg-secondary" v-else>No results</span>
     </div>
 </template>
 <style lang="">
